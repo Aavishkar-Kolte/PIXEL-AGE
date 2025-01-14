17323bfdcf6a27c5eeff29b26b003563f60dcf22
Aavishkar Kolte <aavishkarkolte@gmail.com>
2025-01-14 18:39:04 +0530

    ops: added vercel.json configuration

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,13 @@
 export const configureRoutes = (app) => {
-    app.get('/', (req, res) => {
-        res.sendFile("index.html");
-    })
+  app.get("/", (req, res) => {
+    res.sendFile("index.html");
+  });
 
-    app.get('/*', (req, res) => {
-        res.redirect('/');
-    })
+  app.get("/test", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.get("/*", (req, res) => {
+    res.redirect("/");
+  });
 };
